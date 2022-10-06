ec15693e79d551d6e312f9d727e4d7b888a43880
Hisham Salameh <salamehh@msu.edu>
2022-10-06 14:57:40 -0400

    feat: complete initial setup and begin landing view

---
+++ src/views/LandingView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="landing-view-container">
+    <div class="parent-wrapper">
+      <div class="content-wrapper">
+        <h1 class="hero-text">Its All LeetCoded.</h1>
+        <div class="subcontent-container">
+          <p class="subtext">
+            Leetcode Solutions Visualized and journaled for personal use and use
+            of others.
+          </p>
+          <button class="button">View Solutions</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.landing-view-container {
+  width: 100%;
+  height: 100%;
+  background-image: url("@/assets/blob-background.svg");
+  background-size: cover;
+  background-position: center;
+  .parent-wrapper {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    margin-left: 10rem;
+    font-family: sans-serif;
+    .content-wrapper {
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+      margin-bottom: 2rem;
+      .hero-text {
+        margin: 0;
+        color: #ffffffe0;
+        font-size: 3.5rem;
+      }
+      .subcontent-container {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        width: 75%;
+        .subtext {
+          color: #ffffffe0;
+          margin: 0;
+        }
+        .button {
+          width: 100%;
+          text-align: left;
+          padding-left: 1rem;
+          border: none;
+          border-radius: 0.5rem;
+          height: 2.5rem;
+          background: #1b1f22;
+          color: #ffffffc8;
+          font-weight: 600;
+          font-size: 1.2rem;
+        }
+      }
+    }
+  }
+}
+</style>
