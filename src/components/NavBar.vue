cd8780b5a5205a07005d1748c9a09ebd05e8461c
Hisham Salameh <salamehh@msu.edu>
2022-10-11 18:17:09 -0400

    feat: create nav bar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="nav-parent-container">
+    <nav class="nav-container">
+      <div class="logo-container">
+        <h1>Logo</h1>
+      </div>
+      <div class="login-container">
+        <p>Login</p>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss">
+.nav-parent-container {
+  background-color: #1b1f22;
+  .nav-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    color: white;
+    .logo-container {
+      h1 {
+        margin: 0 0.5rem;
+      }
+    }
+    .login-container {
+      p {
+        margin: 0 0.5rem;
+      }
+    }
+  }
+}
+</style>
