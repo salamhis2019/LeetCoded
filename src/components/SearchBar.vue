6a363b75a1b302f576ba767fdc79ac5f85aee6fa
Hisham Salameh <salamehh@msu.edu>
2022-10-11 23:58:03 -0400

    feat: create search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="search-bar-container">
+    <div class="ui icon input">
+      <input class="prompt" type="text" placeholder="Search..." />
+      <i class="search icon"></i>
+    </div>
+  </div>
+</template>
