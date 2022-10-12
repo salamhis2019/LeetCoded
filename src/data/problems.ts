60d569d811f98afb7e21bd0f87c2d5860639f7d7
Hisham Salameh <salamehh@msu.edu>
2022-10-11 23:01:21 -0400

    feat: create problems ts file

---
+++ src/data/problems.ts
@@ -0,0 +1,36 @@
+export const problems = [
+  {
+    name: "Two Sum",
+    difficulty: "Easy",
+  },
+  {
+    name: "Valid Parens",
+    difficulty: "Easy",
+  },
+  {
+    name: "Add Two Numbers",
+    difficulty: "Medium",
+  },
+  {
+    name: "Reverse Linked List",
+    difficulty: "Easy",
+  },
+  {
+    name: "Median of Two Sorted Arrays",
+    difficulty: "Hard",
+  },
+  {
+    name: "Two Sum",
+    difficulty: "Easy",
+  },
+  {
+    name: "Reverse Integer",
+    difficulty: "Medium",
+  },
+  {
+    name: "Regular Expression Matching",
+    difficulty: "Hard",
+  },
+];
+
+export default problems;
