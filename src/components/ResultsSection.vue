6b823531a3ab0dd05d4c9d17b7d7462ea5b3d53e
Hisham Salameh <salamehh@msu.edu>
2022-10-13 10:37:32 -0400

    refactor: remove pagination styles from results container

--- src/components/ResultsSection.vue
+++ src/components/ResultsSection.vue
@@ -136,50 +136,5 @@ const { currentProblems } = storeToRefs(problemsStore);
       }
     }
   }
-  .pagination-container {
-    display: flex;
-    justify-content: center;
-    gap: 1rem;
-    max-width: 1050px;
-    min-width: 300px;
-    width: 700px;
-    margin-bottom: 2rem;
-    .button {
-      padding: 0.75rem 1rem;
-      color: white;
-      font-weight: 600;
-      font-size: 1.2rem;
-      background: #1b1f22;
-      border: none;
-      border-radius: 8px;
-      cursor: pointer;
-      transition: 0.2s;
-      &:hover {
-        background: #353a3e;
-        color: white;
-      }
-      &:active {
-        background: #ff9900;
-      }
-      &:focus {
-        outline: none;
-        box-shadow: 0px 0px 6px white;
-      }
-    }
-    .active {
-      background: #ff9900;
-      color: #1b1f22;
-    }
-  }
-  @media only screen and (max-width: 750px) {
-    .pagination-container {
-      gap: 0.5rem;
-      width: 300px;
-      .button {
-        padding: 0.5rem 0.8rem;
-        font-size: 0.9rem;
-      }
-    }
-  }
 }
 </style>
