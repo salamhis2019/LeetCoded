8a86ea8cff3e75c546a5495433d8b9fff8eab349
Hisham Salameh <salamehh@msu.edu>
2022-10-15 15:38:26 -0400

    feat: add router link for go back button to the solutions page

--- src/components/ProblemSolution.vue
+++ src/components/ProblemSolution.vue
@@ -3,13 +3,15 @@
     <div class="solution-content-container">
       <div class="header-content">
         <div class="header-left">
-          <h2>{{ currentProblemSolution.name }}</h2>
+          <h3>{{ currentProblemSolution.name }}</h3>
           <DifficultyBadge :difficulty="currentProblemSolution.difficulty">{{
             currentProblemSolution.difficulty
           }}</DifficultyBadge>
         </div>
         <div class="header-right">
-          <button>Go back to Problems</button>
+          <router-link :to="'/solutions/'" class="button solutions">
+            Go Back to Solutions
+          </router-link>
         </div>
       </div>
     </div>
@@ -31,15 +33,34 @@ const { currentProblemSolution }: any = storeToRefs(problemsStore);
   justify-content: center;
   align-items: center;
   color: white;
+  margin: 3rem 0;
   .solution-content-container {
     width: 700px;
     .header-content {
       display: flex;
       justify-content: space-between;
+      align-items: center;
       .header-left {
         display: flex;
+        align-items: center;
+        gap: 1rem;
+        h3 {
+          margin: 0;
+          font-size: 2rem;
+        }
       }
       .header-right {
+        .button {
+          padding: 0.5rem 1rem;
+          background: #353a3e;
+          border-radius: 10px;
+          color: rgba(255, 255, 255, 0.9);
+          font-weight: 600;
+          text-decoration: none;
+          &:hover {
+            background: hsla(207, 8%, 23%, 0.8);
+          }
+        }
       }
     }
   }
