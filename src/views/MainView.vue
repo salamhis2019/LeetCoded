92ec01083f59a03d936e06b764af03db116a53bd
Hisham Salameh <salamehh@msu.edu>
2022-10-15 11:51:25 -0400

    feat: add conditional rendering for problem solutions and listview

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,10 +1,13 @@
 <template>
   <NavBar />
   <div class="parent-container">
-    <div class="mainview-parent-container">
+    <div class="mainview-parent-container" v-if="!param">
       <SearchBar />
       <Results />
       <PaginationBar />
+    </div>
+    <div class="problem-solution-container" v-else>
+      <ProblemSolution />
     </div>
   </div>
 </template>
@@ -13,7 +16,17 @@
 import NavBar from "@/components/NavBar.vue";
 import SearchBar from "@/components/SearchBar.vue";
 import Results from "@/components/ResultsSection.vue";
+import ProblemSolution from "@/components/ProblemSolution.vue";
 import PaginationBar from "@/components/PaginationBar.vue";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const param = computed(() => {
+  console.log(route.params.problem);
+  return route.params.problem;
+});
 </script>
 
 <style lang="scss" setup>
