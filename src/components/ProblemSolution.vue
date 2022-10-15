78c660b4a95fd408e2601fbe8121ae7ad0114d6d
Hisham Salameh <salamehh@msu.edu>
2022-10-15 13:07:48 -0400

    feat: create problems solution component header

--- src/components/ProblemSolution.vue
+++ src/components/ProblemSolution.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1 style="color: white">This is a solution</h1>
+  <div class="solutions-parent-container">
+    <div class="solution-content-container">
+      <div class="header-content">
+        <div class="header-left">
+          <h2>{{ currentProblemSolution.name }}</h2>
+          <p>{{ currentProblemSolution.difficulty }}</p>
+        </div>
+        <div class="header-right">
+          <button>Go back to Problems</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { storeToRefs } from "pinia";
+import useProblemsStore from "@/stores/problems.store";
+
+const problemsStore = useProblemsStore();
+const { currentProblemSolution }: any = storeToRefs(problemsStore);
+</script>
+
+<style lang="scss" scoped>
+.solutions-parent-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  .solution-content-container {
+    width: 700px;
+    .header-content {
+      display: flex;
+      justify-content: space-between;
+      .header-left {
+        display: flex;
+      }
+      .header-right {
+      }
+    }
+  }
+}
+</style>
