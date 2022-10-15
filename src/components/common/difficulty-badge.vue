e6dd6737f1250a113c737bba72f1a82630ae2756
Hisham Salameh <salamehh@msu.edu>
2022-10-15 14:17:59 -0400

    feat: pass down props that determine the style of the badge

--- src/components/common/difficulty-badge.vue
+++ src/components/common/difficulty-badge.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="difficulty-badge">
+  <div
+    class="difficulty-badge"
+    :class="
+      difficulty === 'Easy'
+        ? 'easy'
+        : difficulty === 'Medium'
+        ? 'medium'
+        : 'hard'
+    "
+  >
     <slot></slot>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+defineProps(["difficulty"]);
+</script>
+
+<style lang="scss" scoped>
+.difficulty-badge {
+  font-weight: 600;
+  font-size: 0.9rem;
+  color: rgb(0, 0, 0);
+  padding: 0.3rem 1rem;
+  border-radius: 10px;
+  p {
+    margin: 0;
+  }
+}
+.easy {
+  background: #52b5a3;
+}
+.medium {
+  background: #efbe48;
+}
+.hard {
+  background: #eb4b63;
+}
+</style>
