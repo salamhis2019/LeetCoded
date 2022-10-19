064c39791adcfcb56b9229252def4f14a81ea772
Hisham Salameh <salamehh@msu.edu>
2022-10-19 14:14:13 -0400

    chore: remove old test component and add new component

--- src/components/common/difficulty-badge.vue
+++ src/components/common/difficulty-badge.vue
@@ -1,34 +1,58 @@
 <template>
-  <div
-    class="difficulty-badge"
-    :class="
-      difficulty === 'Easy'
-        ? 'easy'
-        : difficulty === 'Medium'
-        ? 'medium'
-        : 'hard'
-    "
-  >
+  <div :class="clsx(baseClasses, sizeClasses[size], colorClasses[color])">
     <slot></slot>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { defineProps } from "vue";
+import { clsx } from "clsx";
 
-defineProps(["difficulty"]);
+defineProps({
+  size: {
+    type: String,
+    validator: (s: any) => ["primary", "secondary"].includes(s),
+    default: "pill",
+  },
+  color: {
+    type: String,
+    validator: (s: any) => ["easy", "medium", "hard"].includes(s),
+    default: "easy",
+  },
+});
+
+const baseClasses = "difficulty-badge";
+
+const sizeClasses: any = {
+  primary: "primary",
+  secondary: "secondary",
+};
+
+const colorClasses: any = {
+  easy: "easy",
+  medium: "medium",
+  hard: "hard",
+};
 </script>
 
 <style lang="scss" scoped>
 .difficulty-badge {
   font-weight: 600;
-  font-size: 0.9rem;
   color: rgb(0, 0, 0);
-  padding: 0.3rem 1rem;
-  border-radius: 10px;
   p {
     margin: 0;
   }
+}
+.primary {
+  font-size: 1rem;
+  padding: 0.3rem 1rem;
+  border-radius: 10px;
+}
+
+.secondary {
+  font-size: 0.8rem;
+  padding: 0.2rem 0.8rem;
+  border-radius: 8px;
 }
 .easy {
   background: #52b5a3;
@@ -38,12 +62,5 @@ defineProps(["difficulty"]);
 }
 .hard {
   background: #eb4b63;
-}
-
-@media only screen and (max-width: 400px) {
-  .difficulty-badge {
-    padding: 0.2rem 0.8rem;
-    font-size: 0.7rem;
-  }
 }
 </style>
