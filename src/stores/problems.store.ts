bb522748342cb813f1afe9862ce86739283ec6f7
Hisham Salameh <salamehh@msu.edu>
2022-10-25 12:59:02 -0400

    feat: create action that fetches mock data

--- src/stores/problems.store.ts
+++ src/stores/problems.store.ts
@@ -3,10 +3,19 @@ import { defineStore } from "pinia";
 export const useProblemsStore = defineStore("problems", {
   state: () => {
     return {
+      allProblems: [],
       currentProblems: [],
       currentProblemSolution: null,
       showSolution: false,
     };
+  },
+  actions: {
+    async fetchData() {
+      console.log("fetch in progress");
+      const response = await fetch("http://localhost:3000/problems/");
+      const data = await response.json();
+      this.allProblems = data;
+    },
   },
 });
 
