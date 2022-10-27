d6bdaf7a5f202cba2c9b940d22e7c7a024722429
Hisham Salameh <salamehh@msu.edu>
2022-10-27 18:59:46 -0400

    style: adjust focus states for navbar items

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,11 @@
     <nav class="nav-container">
       <div class="logo-container">
         <router-link class="router-link" to="/" @click="logoClicked">
-          <img src="@/assets/leetcoded.png" alt="" />
+          <h1 class="hero-text">
+            <span class="leetcoded-text"
+              >LeetCoded <span class="code-brackets">&lt;></span></span
+            >
+          </h1>
         </router-link>
       </div>
       <button class="login-button">Login</button>
@@ -40,9 +44,33 @@ function logoClicked() {
       .router-link {
         display: flex;
         align-items: center;
-        img {
-          width: 150px;
-          cursor: pointer;
+        text-decoration: none;
+        transition: 0.2s ease;
+        .hero-text {
+          margin: 0;
+          color: #ffffffe0;
+          font-size: 1.5rem;
+          font-weight: 300;
+          .leetcoded-text {
+            display: flex;
+            align-items: center;
+            gap: 0.5rem;
+            font-style: italic;
+            font-weight: 600;
+          }
+          .code-brackets {
+            color: orange;
+            font-size: 2rem;
+            font-weight: 500;
+          }
+        }
+        &:hover {
+          opacity: 0.8;
+        }
+        &:focus {
+          outline: none;
+          transform: scale(1.05);
+          text-shadow: 2px 2px 0px rgba(255, 132, 0, 0.8);
         }
       }
     }
@@ -58,6 +86,11 @@ function logoClicked() {
       cursor: pointer;
       &:hover {
         box-shadow: 0px 0px 2px orange;
+      }
+      &:focus {
+        outline: none;
+        transform: scale(1.05);
+        box-shadow: 2px 2px 2px 0px rgba(255, 132, 0, 0.8);
       }
     }
   }
