2cf234877e5901964b4bba0e52947c595a7c6ac1
Hisham Salameh <salamehh@msu.edu>
2022-10-27 10:11:25 -0400

    feat: add v-click away directive for dropdown

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import VueClickAway from "vue3-click-away";
 import router from "./router";
 
 const app = createApp(App);
 const pinia = createPinia();
 
+app.use(VueClickAway);
 app.use(pinia);
 app.use(router).mount("#app");
