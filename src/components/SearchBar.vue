83c9c23a671490d97b449a0da5a433c68ac2a0dd
Hisham Salameh <salamehh@msu.edu>
2022-10-31 11:39:29 -0400

    fix: refactor search bar with prettier tailwind

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,11 +2,13 @@
   <div
     class="search-bar-parent-container my-0 mx-8 flex h-36 items-center justify-center"
   >
-    <div class="search-bar-container min-w-300 box-border flex h-12 py-2 px-4">
-      <form class="input-container">
+    <div
+      class="search-bar-container min-w-300 box-border flex h-12 rounded-xl bg-[#1b1f22] py-2 px-4"
+    >
+      <form class="input-container flex grow">
         <input
           v-model="inputText"
-          class="search-bar"
+          class="search-bar h-full w-full rounded-xl border-none bg-transparent text-base text-white/90 placeholder-white outline-none"
           type="text"
           placeholder="Search..."
           @keydown.enter.prevent="
@@ -14,13 +16,18 @@
           "
         />
       </form>
-      <div class="search-bar-right">
-        <div class="sort dropdown" v-click-away="onClickAway">
+      <div class="search-bar-right flex">
+        <div
+          class="sort dropdown relative flex items-center text-base text-white"
+          v-click-away="onClickAway"
+        >
           <div class="dropdown-button" @click="showDropdown = !showDropdown">
-            <button class="sort-button">
+            <button
+              class="sort-button ease box-border flex cursor-pointer items-center gap-2 border-none bg-transparent text-white/90 duration-200 hover:opacity-80 focus:rounded-lg focus:bg-[#353a3e] focus:outline-none"
+            >
               Filter By
               <i
-                class="fa fa-angle-down"
+                class="fa fa-angle-down cursor-pointer text-2xl"
                 :class="{ 'chevron-clicked': showDropdown }"
               ></i>
             </button>
@@ -54,72 +61,13 @@ function onClickAway() {
 .search-bar-parent-container {
   .search-bar-container {
     width: 600px;
-    background: #1b1f22;
-    border-radius: 12px;
-    .input-container {
-      display: flex;
-      flex-grow: 4;
-      height: 100%;
-      .search-bar {
-        height: calc(100% - 2px);
-        width: 100%;
-        background: none;
-        border: none;
-        border-radius: 12px;
-        outline: none;
-        font-size: 1.2rem;
-        color: white;
-        &::placeholder {
-          color: hsla(0, 0%, 100%, 0.9);
-        }
-      }
-    }
     .search-bar-right {
-      display: flex;
-      .sort {
-        display: flex;
-        align-items: center;
-        color: hsla(0, 0%, 100%, 0.9);
-        p {
-          font-size: 1.2rem;
-          margin: 0;
-        }
-      }
       .dropdown-button {
-        display: flex;
-        box-sizing: border-box;
         .sort-button {
-          display: flex;
-          align-items: center;
-          gap: 0.5rem;
-          font-family: "Encode Sans", sans-serif;
-          font-size: 1rem;
-          color: white;
-          background: none;
-          border: none;
-          cursor: pointer;
-          transition: 0.2s ease;
-          .fa-angle-down {
-            font-size: 1.5rem;
-            cursor: pointer;
-          }
           .chevron-clicked {
             transform: rotate(270deg);
           }
-          &:focus {
-            outline: none;
-            padding: 0.5rem 1rem;
-            border-radius: 8px;
-            background: #353a3e;
-            box-shadow: 0 2px 5px 0 rgba(255, 255, 255, 0.103);
-          }
         }
-        &:hover {
-          opacity: 0.8;
-        }
-      }
-      .dropdown {
-        position: relative;
       }
     }
   }
