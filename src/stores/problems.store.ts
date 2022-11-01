9f13114ea1f515f8f58a90f49a50fdeec4d11564
Hisham Salameh <salamehh@msu.edu>
2022-11-01 13:37:43 -0400

    feat: implement data loading state and add delay for assignment

--- src/stores/problems.store.ts
+++ src/stores/problems.store.ts
@@ -7,37 +7,60 @@ export const useProblemsStore = defineStore("problems", {
       currentProblems: [],
       currentProblemSolution: null,
       showSolution: false,
+      dataLoading: false,
     };
   },
   actions: {
     async fetchData() {
+      this.dataLoading = true;
       try {
         const response = await fetch("http://localhost:3000/problems/");
         const data = await response.json();
         this.allProblems = data;
+        setTimeout(() => {
+          this.dataLoading = false;
+        }, 1000);
       } catch (error) {
         console.error(error);
       }
     },
     async searchData(searchText: any) {
-      const response = await fetch(
-        `http://localhost:3000/problems?param=${searchText}`
-      );
-      const data = await response.json();
-      this.allProblems = data;
-      if (data.length === 0) {
-        this.fetchData();
-      }
-    },
-    async filterData(difficulty: any) {
-      if (difficulty !== "All") {
+      this.dataLoading = true;
+      try {
         const response = await fetch(
-          `http://localhost:3000/problems?difficulty=${difficulty}`
+          `http://localhost:3000/problems?param=${searchText}`
         );
         const data = await response.json();
         this.allProblems = data;
-      } else {
-        this.fetchData();
+        if (data.length === 0) {
+          this.fetchData();
+        }
+        setTimeout(() => {
+          this.dataLoading = false;
+        }, 1000);
+      } catch (error) {
+        this.dataLoading = false;
+        console.log(error);
+      }
+    },
+    async filterData(difficulty: any) {
+      this.dataLoading = true;
+      try {
+        if (difficulty !== "All") {
+          const response = await fetch(
+            `http://localhost:3000/problems?difficulty=${difficulty}`
+          );
+          const data = await response.json();
+          this.allProblems = data;
+        } else {
+          this.fetchData();
+        }
+        setTimeout(() => {
+          this.dataLoading = false;
+        }, 1000);
+      } catch (error) {
+        this.dataLoading = false;
+        console.error(error);
       }
     },
   },
