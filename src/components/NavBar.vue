617f870ca4413ca430def19e8b436726bc3d62cd
Hisham Salameh <salamehh@msu.edu>
2022-11-09 20:27:15 -0500

    feat: create is logged in state and allow the user to sign in and out

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -22,22 +22,61 @@
       </div>
       <button
         @click="showLoginWindow = true"
+        v-if="!isLoggedIn"
         class="login-button ease cursor-pointer rounded-lg border-none bg-[#353a3e] py-1.5 px-6 text-base font-semibold text-white/90 duration-200 hover:scale-105 focus:scale-105 focus:shadow-lg focus:outline-0"
       >
         Login
       </button>
+      <div v-else class="signed-in-container">
+        <button
+          class="flex items-center gap-2 text-white"
+          @click="handleSignout"
+        >
+          Hello, <span class="font-bold italic">{{ currentUser }}</span>
+          <i
+            class="fa fa-angle-down cursor-pointer text-2xl font-bold"
+            data-v-71fbddf2
+            aria-hidden="true"
+          ></i>
+        </button>
+      </div>
     </nav>
   </div>
 </template>
 
 <script lang="ts" setup>
-import useProblemsStore from "@/stores/problems.store";
+import { onMounted, ref } from "vue";
 import { storeToRefs } from "pinia";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import useProblemsStore from "@/stores/problems.store";
 
 const problemsStore = useProblemsStore();
 const { showSolution, showLoginWindow } = storeToRefs(problemsStore);
 
+const isLoggedIn = ref(false);
+
 function logoClicked() {
   showSolution.value = false;
 }
+
+const handleSignout = () => {
+  signOut(auth).then(() => {
+    console.log("signed out");
+  });
+};
+
+const currentUser = ref<any>("");
+let auth: any;
+onMounted(() => {
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log("user: ", user.email);
+      currentUser.value = user.email;
+      isLoggedIn.value = true;
+    } else {
+      isLoggedIn.value = false;
+    }
+  });
+});
 </script>
