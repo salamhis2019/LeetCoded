c427cf9fa771a79629c1a28254ff80aa1541aea7
Hisham Salameh <salamehh@msu.edu>
2022-11-10 15:22:38 -0500

    fix: remove custom checkbox

--- src/components/ResultsSection.vue
+++ src/components/ResultsSection.vue
@@ -36,11 +36,8 @@
               {{ difficulty }}
             </DifficultyBadge>
           </div>
-          <label class="checkbox bounce">
-            <input type="checkbox" />
-            <svg viewBox="0 0 21 21">
-              <polyline points="5 10.75 8.5 14.25 16 6"></polyline>
-            </svg>
+          <label class="checkbox flex items-center accent-[#52b5a3]">
+            <input type="checkbox" class="h-6 w-6 cursor-pointer" />
           </label>
         </div>
       </div>
@@ -110,111 +107,6 @@ function setCurrentProblem(index: any) {
         }
       }
     }
-  }
-}
-.checkbox {
-  --background: #fff;
-  --border: #d1d6ee;
-  --border-hover: #bbc1e1;
-  --border-active: #52b5a3;
-  --tick: #fff;
-  position: relative;
-  input,
-  svg {
-    width: 21px;
-    height: 21px;
-    display: block;
-  }
-  input {
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    position: relative;
-    outline: none;
-    background: var(--background);
-    border: none;
-    margin: 0;
-    padding: 0;
-    cursor: pointer;
-    border-radius: 4px;
-    transition: box-shadow 0.2s;
-    box-shadow: inset 0 0 0 var(--s, 1px) var(--b, var(--border));
-    &:hover {
-      --s: 2px;
-      --b: var(--border-hover);
-    }
-    &:checked {
-      --b: var(--border-active);
-    }
-  }
-  svg {
-    pointer-events: none;
-    fill: none;
-    stroke-width: 2px;
-    stroke-linecap: round;
-    stroke-linejoin: round;
-    stroke: var(--stroke, var(--border-active));
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 21px;
-    height: 21px;
-    transform: scale(var(--scale, 1)) translateZ(0);
-  }
-  &.path {
-    input {
-      &:checked {
-        --s: 2px;
-        transition-delay: 0.2s;
-        & + svg {
-          --a: 16.1 86.12;
-          --o: 102.22;
-        }
-      }
-    }
-    svg {
-      stroke-dasharray: var(--a, 86.12);
-      stroke-dashoffset: var(--o, 86.12);
-      transition: stroke-dasharray 0.2s, stroke-dashoffset 0.2s;
-    }
-  }
-  &.bounce {
-    --stroke: var(--tick);
-    input {
-      &:checked {
-        --s: 11px;
-        & + svg {
-          animation: bounce 0.2s linear forwards 0.2s;
-        }
-      }
-    }
-    svg {
-      --scale: 0;
-    }
-  }
-}
-
-@keyframes bounce {
-  50% {
-    transform: scale(1.2);
-  }
-  75% {
-    transform: scale(0.9);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
-
-html {
-  box-sizing: border-box;
-  -webkit-font-smoothing: antialiased;
-}
-
-* {
-  box-sizing: inherit;
-  &:before,
-  &:after {
-    box-sizing: inherit;
   }
 }
 </style>
