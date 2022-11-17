8a4646781cc7d5f6c983a5d90d94b9b6dacb3548
Hisham Salameh <salamehh@msu.edu>
2022-11-16 23:02:22 -0500

    style: finish footer styling for hover and focus states

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -23,10 +23,24 @@
     </div>
     <hr class="my-6 w-full text-center opacity-20" />
     <div class="socials flex items-center gap-6 text-3xl text-white/90">
-      <i
-        class="fa fa-linkedin flex h-11 w-11 items-center justify-center rounded-full bg-white/90 text-[#1b1f22]"
-      ></i>
-      <i class="fa fa-github text-5xl"></i>
+      <a
+        href="https://www.linkedin.com/in/hishamsalameh"
+        target="blank"
+        class="duration-200 focus:scale-110 focus:outline-0"
+      >
+        <i
+          class="fa fa-linkedin flex h-11 w-11 cursor-pointer items-center justify-center rounded-full bg-white/90 text-[#1b1f22] duration-200 ease-in-out hover:scale-110 hover:bg-[#52b5a3] focus:bg-[#52b5a3]"
+        ></i>
+      </a>
+      <a
+        href="https://www.github.com/salamhis2019"
+        target="blank"
+        class="duration-100 focus:scale-110 focus:text-[#52b5a3] focus:outline-0"
+      >
+        <i
+          class="fa fa-github cursor-pointer text-5xl duration-200 ease-in-out hover:scale-110 hover:text-[#52b5a3]"
+        ></i>
+      </a>
     </div>
     <p class="mt-4 text-sm text-white/40">&copy; 2022 - Hisham Salameh</p>
   </div>
