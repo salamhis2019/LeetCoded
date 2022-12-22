2cfc324c8679865ef3b928b8e60f26e9fe25dd1d
Hisham Salameh <salamehh@msu.edu>
2022-12-22 00:30:30 -0500

    feat: complete checking logic in checkbox

---
+++ src/components/common/problem-checkbox.vue
@@ -0,0 +1,9 @@
+<template>
+  <input type="checkbox" class="h-6 w-6 cursor-pointer" v-model="checked" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const checked = ref();
+</script>
